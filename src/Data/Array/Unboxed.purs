0f347082d2a2ad258499d06b61b81932d5f0af66
rightfold <rightfold@gmail.com>
2017-02-20 23:29:48 +0100

    Add function to convert mutable arrays to immutable arrays

--- src/Data/Array/Unboxed.purs
+++ src/Data/Array/Unboxed.purs
@@ -1,13 +1,15 @@
 module Data.Array.Unboxed
   ( UnboxedArray
 
+  , build
   , fill
 
   , index
   , (!!)
   ) where
 
-import Control.Monad.ST (pureST)
+import Control.Monad.Eff (Eff)
+import Control.Monad.ST (ST, pureST)
 import Data.Array.Unboxed.ST (class STUnboxedArray)
 import Data.Array.Unboxed.ST as ST
 import Data.Maybe (Maybe)
@@ -21,12 +23,21 @@ foreign import data UnboxedArray :: (Type -> Type) -> Type -> Type
 
 --------------------------------------------------------------------------------
 
+-- | Create a new immutable array from a mutable array. The conversion is
+-- | instantanious.
+build
+  :: ∀ as a
+   . (STUnboxedArray as a)
+  => (∀ r e. Eff (st :: ST r | e) (as r))
+  -> UnboxedArray as a
+build go = pureST do
+  array <- go
+  pure $ (unsafeCoerce :: ∀ r. as r -> UnboxedArray as a) array
+
 -- | *O(n)* Create a new array of a certain length with all elements set to a
 -- | certain value.
 fill :: ∀ as a. (STUnboxedArray as a) => Int -> a -> UnboxedArray as a
-fill length value = pureST do
-  array <- ST.new length value
-  pure $ (unsafeCoerce :: ∀ r. as r -> UnboxedArray as a) array
+fill length value = build (ST.new length value)
 
 --------------------------------------------------------------------------------
 
