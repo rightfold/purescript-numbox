5b25673d32c2313023b04c89c9a3c276a87775b5
rightfold <rightfold@gmail.com>
2017-02-20 14:36:10 +0100

    Add complex arithmetic

--- src/Data/Complex.purs
+++ src/Data/Complex.purs
@@ -8,6 +8,19 @@ data Complex = Complex Number Number
 
 derive instance eqComplex :: Eq Complex
 
+instance semiringComplex :: Semiring Complex where
+  zero = Complex 0.0 0.0
+  one  = Complex 1.0 0.0
+  add (Complex real1 imag1) (Complex real2 imag2) =
+    Complex (real1 + real2) (imag1 + imag2)
+  mul (Complex real1 imag1) (Complex real2 imag2) =
+    Complex (real1 * real2 - imag1 * imag2)
+            (real1 * imag2 + imag1 * real2)
+
+instance ringComplex :: Ring Complex where
+  sub (Complex real1 imag1) (Complex real2 imag2) =
+    Complex (real1 - real2) (imag1 - imag2)
+
 instance showComplex :: Show Complex where
   show (Complex real imag) =
     "(Complex " <> show real <> " " <> show imag <> ")"
